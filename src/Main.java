80eb28eecf5ea5bc54f302a8dc6987c9b4c10fc3
GidJoe <winterm@bfwob.de>
2023-01-10 07:37:47 +0100

    RLS

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("n?");
-        int anzahl = Integer.parseInt(reader.readLine());
-        boolean[] n = new boolean[anzahl + 1];
+        Scanner myscanner = new Scanner(System.in);
+        int bereich = myscanner.nextInt();
+        boolean[] n = new boolean[bereich + 1];
 
 
-        for (int a = 2; a < anzahl; a++) {
-            for (int b = 2; b < anzahl; b++) {
-                if ((a*b)<n.length) {n[a * b] = true;}
+
+        // Wir brauchen zwei Schleifen, weil wir hier wieder a*b rechnen
+        // Die Position in einem Boolean kann durch eine Rechnung bestimmt werden
+        // for (int b = 2; b < bereich; b++) kann ersetzt werden durch
+        // b <= (bereich/b)
+
+        for (int a = 2; a < bereich; a++) {
+
+            for (int b = 2; b < bereich; b++) {
+                if ((a * b) < n.length) {
+                    n[a * b] = true;
+                }
             }
         }
 
+        // Ausgabe
+        // !n[i] ist eine kurze Version von n[i] = false;
+
         System.out.println("Das sind deine Primzahlen");
         System.out.println("");
-        for (int i = 1; i <= anzahl; i++) {
+        for (int i = 1; i <= bereich; i++) {
             if (!n[i]) {
                 System.out.print(i + " ");
             }
