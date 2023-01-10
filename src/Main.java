8c0f30dd40b241ba5bf1d3d9a931770f282cee72
GidJoe <winterm@bfwob.de>
2023-01-10 14:50:32 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Scanner myscanner = new Scanner(System.in);
         int bereich = myscanner.nextInt();
@@ -30,8 +28,8 @@ public class Main {
         // !n[i] ist eine kurze Version von n[i] = false;
 
         System.out.println("Das sind deine Primzahlen");
-        System.out.println("");
-        for (int i = 2; i <= bereich; i++) {
+        System.out.println();
+        for (int i = 4; i <= bereich; i++) {
             if (!n[i]) {
                 System.out.print(i + " ");
             }
